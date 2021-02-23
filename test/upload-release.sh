9f4908312a5f21fdec977421ea0d5e475d0ab1e4
Arthur Chaloin <arthur.chaloin@gmail.com>
2021-02-23 17:04:12 +0100

    chore(ci): fllow github auth redirect

--- test/upload-release.sh
+++ test/upload-release.sh
@@ -49,7 +49,7 @@ fi
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
 # Read asset tags.
-response=$(curl -sH "$AUTH" $GH_TAGS)
+response=$(curl -sLH "$AUTH" $GH_TAGS)
 
 # Get ID of the asset based on given filename.
 eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')

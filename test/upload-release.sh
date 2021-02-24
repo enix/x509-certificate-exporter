2683bc40047b55da7ac97ab3b425f96a4e93bdae
Arthur Chaloin <arthur.chaloin@gmail.com>
2021-02-24 11:33:41 +0100

    chore(ci): quote release url

--- test/upload-release.sh
+++ test/upload-release.sh
@@ -62,4 +62,4 @@ echo "Uploading asset... "
 GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
 
 echo $GH_ASSET
-curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "Authorization: token $github_api_token" -H "Content-Type: application/octet-stream" $GH_ASSET
+curl "$GITHUB_OAUTH_BASIC" --data-binary @"$filename" -H "Authorization: token $github_api_token" -H "Content-Type: application/octet-stream" "$GH_ASSET"

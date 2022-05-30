a403201af0c5dc1993f5ae079c701b91e2a9b338
Arthur Chaloin <arthur.chaloin@gmail.com>
2022-05-30 16:31:44 +0200

    feat(x509-certificate-exporter): add ci script

--- ci.sh
+++ ci.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-echo $0 $1 $2
+version="$1"
+notes="$2"
+containsSecurityUpdates="$3"
+
+prerelease="false"
+versionComponents=(${version//-/ })
+
+if [ ! -z "${versionComponents[1]}" ]; then
+	prerelease="true"
+fi
+
+yq -i ".version = \"$version\"" Chart.yaml
+yq -i ".appVersion = \"$version\"" Chart.yaml
+yq -i ".annotations[\"artifacthub.io/prerelease\"] = \"$prerelease\"" Chart.yaml
+yq -i ".annotations[\"artifacthub.io/containsSecurityUpdates\"] = \"$containsSecurityUpdates\"" Chart.yaml
+yq -i ".annotations[\"artifacthub.io/changes\"] = \"$(echo "$notes" | sed "s/\\\"/\\\\\"/g")\"" Chart.yaml
